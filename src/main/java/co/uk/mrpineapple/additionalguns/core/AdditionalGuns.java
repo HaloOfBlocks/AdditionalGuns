bff2b74d339f1ef37847574392ea8af074f8205f
Mr-Pineapple <zackbird1@outlook.com>
2021-05-30 21:14:56 +0100

    Initial commit

---
+++ src/main/java/co/uk/mrpineapple/additionalguns/core/AdditionalGuns.java
@@ -0,0 +1,8 @@
+package com.example.examplemod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(AdditionalGuns.ID)
+public class AdditionalGuns {
+    public static final String ID = "additionalguns";
+}
