0942984630696b1bbbd396d8f796b677d708cafc
Mr-Pineapple <zackbird1@outlook.com>
2021-06-01 20:45:48 +0100

    Added Basic Stock
    Model: ThatAvocado_Boi

--- src/main/java/co/uk/mrpineapple/additionalguns/core/registry/ItemRegistry.java
+++ src/main/java/co/uk/mrpineapple/additionalguns/core/registry/ItemRegistry.java
@@ -4,7 +4,11 @@ import co.uk.mrpineapple.additionalguns.core.AdditionalGuns;
 import com.mrcrayfish.guns.GunMod;
 import com.mrcrayfish.guns.common.GunModifiers;
 import com.mrcrayfish.guns.item.ScopeItem;
+import com.mrcrayfish.guns.item.StockItem;
+import com.mrcrayfish.guns.item.UnderBarrelItem;
 import com.mrcrayfish.guns.item.attachment.impl.Scope;
+import com.mrcrayfish.guns.item.attachment.impl.Stock;
+import com.mrcrayfish.guns.item.attachment.impl.UnderBarrel;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -16,4 +20,7 @@ public class ItemRegistry {
 
     /* Scope Attachments */
     public static final RegistryObject<Item> HOLO_SCOPE = ITEMS.register("holo_scope", () -> new ScopeItem(Scope.create(0.25F, 1.625F, GunModifiers.SLOW_ADS).viewFinderOffset(0.3), new Item.Properties().maxStackSize(1).group(GunMod.GROUP)));
+
+    /* Stock Attachments */
+    public static final RegistryObject<Item> BASIC_STOCK = ITEMS.register("basic_stock", () -> new StockItem(Stock.create(GunModifiers.SLOW_ADS), new Item.Properties().maxStackSize(1).group(GunMod.GROUP), false));
 }
