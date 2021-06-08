2358ff8f1e0d6bbbaa724d79fe944187d4d3f053
Mr-Pineapple <zackbird1@outlook.com>
2021-06-08 17:47:36 +0100

    Added vector & updated mammoth model - Forgot to commit

--- src/main/java/co/uk/mrpineapple/additionalguns/core/registry/ItemRegistry.java
+++ src/main/java/co/uk/mrpineapple/additionalguns/core/registry/ItemRegistry.java
@@ -3,6 +3,7 @@ package co.uk.mrpineapple.additionalguns.core.registry;
 import co.uk.mrpineapple.additionalguns.core.AdditionalGuns;
 import com.mrcrayfish.guns.GunMod;
 import com.mrcrayfish.guns.common.GunModifiers;
+import com.mrcrayfish.guns.item.GunItem;
 import com.mrcrayfish.guns.item.ScopeItem;
 import com.mrcrayfish.guns.item.StockItem;
 import com.mrcrayfish.guns.item.UnderBarrelItem;
@@ -17,6 +18,10 @@ import net.minecraftforge.registries.ForgeRegistries;
 //CLASS FOR REGISTERING GUNS
 public class ItemRegistry {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, AdditionalGuns.ID);
+
+    /* Guns */
+    public static final RegistryObject<Item> MAMMOTH = ITEMS.register("mammoth", () -> new GunItem(new Item.Properties().maxStackSize(1).group(GunMod.GROUP)));
+    public static final RegistryObject<Item> VECTOR = ITEMS.register("vector", () -> new GunItem(new Item.Properties().maxStackSize(1).group(GunMod.GROUP)));
 
     /* Scope Attachments */
     public static final RegistryObject<Item> HOLO_SCOPE = ITEMS.register("holo_scope", () -> new ScopeItem(Scope.create(0.25F, 1.625F, GunModifiers.SLOW_ADS).viewFinderOffset(0.3), new Item.Properties().maxStackSize(1).group(GunMod.GROUP)));
