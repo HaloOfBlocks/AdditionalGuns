df67ba5d642f2dc7d12ce56fb5a452540f9be170
Mr-Pineapple <zackbird1@outlook.com>
2021-08-09 18:06:21 +0100

    :art: Added genericProperties variable
    Should probably just make some registry methods tbh

--- src/main/java/co/uk/mrpineapple/additionalguns/core/registry/ItemRegistry.java
+++ src/main/java/co/uk/mrpineapple/additionalguns/core/registry/ItemRegistry.java
@@ -17,41 +17,42 @@ import net.minecraftforge.registries.ForgeRegistries;
 //CLASS FOR REGISTERING GUNS
 public class ItemRegistry {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, AdditionalGuns.ID);
+    public static final Item.Properties genericProperties = new Item.Properties().maxStackSize(1).group(GunMod.GROUP);
 
     /* Guns */
-    public static final RegistryObject<Item> MAMMOTH = ITEMS.register("mammoth", () -> new UndyeableGunItem(new Item.Properties().maxStackSize(1).group(GunMod.GROUP)));
-    public static final RegistryObject<Item> VECTOR = ITEMS.register("vector", () -> new UndyeableGunItem(new Item.Properties().maxStackSize(1).group(GunMod.GROUP)));
-    public static final RegistryObject<Item> NINE_A_NINE_ONE = ITEMS.register("9a91", () -> new GunItem(new Item.Properties().maxStackSize(1).group(GunMod.GROUP)));
-    public static final RegistryObject<Item> ACE_OF_SPADES = ITEMS.register("ace_of_spades", () -> new UndyeableGunItem(new Item.Properties().maxStackSize(1).group(GunMod.GROUP)));
-    public static final RegistryObject<Item> G_ELEVEN = ITEMS.register("g11", () -> new GunItem(new Item.Properties().maxStackSize(1).group(GunMod.GROUP)));
-    public static final RegistryObject<Item> VINTOREZ = ITEMS.register("vintorez", () -> new UndyeableGunItem(new Item.Properties().maxStackSize(1).group(GunMod.GROUP)));
-    public static final RegistryObject<Item> VAL = ITEMS.register("val", () -> new UndyeableGunItem(new Item.Properties().maxStackSize(1).group(GunMod.GROUP)));
-    public static final RegistryObject<Item> AUG = ITEMS.register("aug", () -> new GunItem(new Item.Properties().maxStackSize(1).group(GunMod.GROUP)));
-    public static final RegistryObject<Item> FN_TWO_THOUSAND = ITEMS.register("fn2000", () -> new UndyeableGunItem(new Item.Properties().maxStackSize(1).group(GunMod.GROUP)));
-    public static final RegistryObject<Item> PP_NINETEEN = ITEMS.register("pp_19", () -> new UndyeableGunItem(new Item.Properties().maxStackSize(1).group(GunMod.GROUP)));
-    public static final RegistryObject<Item> MAT_FORTY_NINE = ITEMS.register("mat_49", () -> new GunItem(new Item.Properties().maxStackSize(1).group(GunMod.GROUP)));
-    public static final RegistryObject<Item> SCHWARZLOSE = ITEMS.register("schwarzlose", () -> new GunItem(new Item.Properties().maxStackSize(1).group(GunMod.GROUP)));
-    public static final RegistryObject<Item> OTS_THREE = ITEMS.register("ots_03", () -> new UndyeableGunItem(new Item.Properties().maxStackSize(1).group(GunMod.GROUP)));
-    public static final RegistryObject<Item> AK_HUNDRED_FIVE = ITEMS.register("ak105", () -> new UndyeableGunItem(new Item.Properties().maxStackSize(1).group(GunMod.GROUP)));
-    public static final RegistryObject<Item> RAVENS_CLAW = ITEMS.register("ravens_claw", () -> new UndyeableGunItem(new Item.Properties().maxStackSize(1).group(GunMod.GROUP)));
-    public static final RegistryObject<Item> BANSHEE = ITEMS.register("banshee", () -> new UndyeableGunItem(new Item.Properties().maxStackSize(1).group(GunMod.GROUP)));
+    public static final RegistryObject<Item> MAMMOTH = ITEMS.register("mammoth", () -> new UndyeableGunItem(genericProperties));
+    public static final RegistryObject<Item> VECTOR = ITEMS.register("vector", () -> new UndyeableGunItem(genericProperties));
+    public static final RegistryObject<Item> NINE_A_NINE_ONE = ITEMS.register("9a91", () -> new GunItem(genericProperties));
+    public static final RegistryObject<Item> ACE_OF_SPADES = ITEMS.register("ace_of_spades", () -> new UndyeableGunItem(genericProperties));
+    public static final RegistryObject<Item> G_ELEVEN = ITEMS.register("g11", () -> new GunItem(genericProperties));
+    public static final RegistryObject<Item> VINTOREZ = ITEMS.register("vintorez", () -> new UndyeableGunItem(genericProperties));
+    public static final RegistryObject<Item> VAL = ITEMS.register("val", () -> new UndyeableGunItem(genericProperties));
+    public static final RegistryObject<Item> AUG = ITEMS.register("aug", () -> new GunItem(genericProperties));
+    public static final RegistryObject<Item> FN_TWO_THOUSAND = ITEMS.register("fn2000", () -> new UndyeableGunItem(genericProperties));
+    public static final RegistryObject<Item> PP_NINETEEN = ITEMS.register("pp_19", () -> new UndyeableGunItem(genericProperties));
+    public static final RegistryObject<Item> MAT_FORTY_NINE = ITEMS.register("mat_49", () -> new GunItem(genericProperties));
+    public static final RegistryObject<Item> SCHWARZLOSE = ITEMS.register("schwarzlose", () -> new GunItem(genericProperties));
+    public static final RegistryObject<Item> OTS_THREE = ITEMS.register("ots_03", () -> new UndyeableGunItem(genericProperties));
+    public static final RegistryObject<Item> AK_HUNDRED_FIVE = ITEMS.register("ak105", () -> new UndyeableGunItem(genericProperties));
+    public static final RegistryObject<Item> RAVENS_CLAW = ITEMS.register("ravens_claw", () -> new UndyeableGunItem(genericProperties));
+    public static final RegistryObject<Item> BANSHEE = ITEMS.register("banshee", () -> new UndyeableGunItem(genericProperties));
 
     /* Scope Attachments */
-    public static final RegistryObject<Item> HOLO_SCOPE = ITEMS.register("holo_scope", () -> new ScopeItem(Scope.create(0.25F, 1.375F, GunModifiers.SLOW_ADS).viewFinderOffset(0.3), new Item.Properties().maxStackSize(1).group(GunMod.GROUP), false));
-    public static final RegistryObject<Item> ZERKALO_SCOPE = ITEMS.register("zerkalo_scope", () -> new ScopeItem(Scope.create(0.025F, 1.275F, GunModifiers.SLOWER_ADS).viewFinderOffset(0.3), new Item.Properties().maxStackSize(1).group(GunMod.GROUP), false));
+    public static final RegistryObject<Item> HOLO_SCOPE = ITEMS.register("holo_scope", () -> new ScopeItem(Scope.create(0.25F, 1.375F, GunModifiers.SLOW_ADS).viewFinderOffset(0.3), genericProperties, false));
+    public static final RegistryObject<Item> ZERKALO_SCOPE = ITEMS.register("zerkalo_scope", () -> new ScopeItem(Scope.create(0.025F, 1.275F, GunModifiers.SLOWER_ADS).viewFinderOffset(0.3), genericProperties, false));
 
     /* Stock Attachments */
-    public static final RegistryObject<Item> BASIC_STOCK = ITEMS.register("basic_stock", () -> new StockItem(Stock.create(GunModifiers.SLOW_ADS), new Item.Properties().maxStackSize(1).group(GunMod.GROUP), false));
+    public static final RegistryObject<Item> BASIC_STOCK = ITEMS.register("basic_stock", () -> new StockItem(Stock.create(GunModifiers.SLOW_ADS), genericProperties, false));
     // Removed until fixed!
-    //public static final RegistryObject<Item> FLIMSY_STOCK = ITEMS.register("flimsy_stock", () -> new StockItem(Stock.create(GunModifiers.SLOWER_ADS), new Item.Properties().maxStackSize(1).group(GunMod.GROUP), true));
-    //public static final RegistryObject<Item> MAMMOTH_STOCK = ITEMS.register("mammoth_stock", () -> new StockItem(Stock.create(GunModifiers.SLOW_ADS), new Item.Properties().maxStackSize(1).group(GunMod.GROUP), false));
-    //public static final RegistryObject<Item> VINTOREZ_STOCK = ITEMS.register("vintorez_stock", () -> new StockItem(Stock.create(GunModifiers.SLOW_ADS), new Item.Properties().maxStackSize(1).group(GunMod.GROUP), false));
+    //public static final RegistryObject<Item> FLIMSY_STOCK = ITEMS.register("flimsy_stock", () -> new StockItem(Stock.create(GunModifiers.SLOWER_ADS), genericProperties, true));
+    //public static final RegistryObject<Item> MAMMOTH_STOCK = ITEMS.register("mammoth_stock", () -> new StockItem(Stock.create(GunModifiers.SLOW_ADS), genericProperties, false));
+    //public static final RegistryObject<Item> VINTOREZ_STOCK = ITEMS.register("vintorez_stock", () -> new StockItem(Stock.create(GunModifiers.SLOW_ADS), genericProperties, false));
 
     /* Barrel Attachments */
-    public static final RegistryObject<Item> MUZZLE_BRAKE = ITEMS.register("muzzle_brake", () -> new BarrelItem(Barrel.create(8.0f, GunModifiers.REDUCED_RECOIL), new Item.Properties().maxStackSize(1).group(GunMod.GROUP), false));
+    public static final RegistryObject<Item> MUZZLE_BRAKE = ITEMS.register("muzzle_brake", () -> new BarrelItem(Barrel.create(8.0f, GunModifiers.REDUCED_RECOIL), genericProperties, false));
 
     /* Under Barrel Attachments */
-    public static final RegistryObject<Item> ANGLED_GRIP = ITEMS.register("angled_grip", () -> new UnderBarrelItem(UnderBarrel.create(GunModifiers.LIGHT_RECOIL), new Item.Properties().maxStackSize(1).group(GunMod.GROUP)));
+    public static final RegistryObject<Item> ANGLED_GRIP = ITEMS.register("angled_grip", () -> new UnderBarrelItem(UnderBarrel.create(GunModifiers.LIGHT_RECOIL), genericProperties));
 
     /* Ammunition */
     public static final RegistryObject<Item> AMMO_NINE = ITEMS.register("ammo_9", () -> new AmmoItem(new Item.Properties().group(GunMod.GROUP)));
