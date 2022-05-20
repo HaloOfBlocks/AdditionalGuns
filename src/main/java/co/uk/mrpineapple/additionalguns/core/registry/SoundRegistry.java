c92af4389650b50ffe97a9491b04328bd695834d
Autovw <68228006+Autovw@users.noreply.github.com>
2022-05-20 11:04:03 +0200

    ✨ Added custom sounds for AK's

--- src/main/java/co/uk/mrpineapple/additionalguns/core/registry/SoundRegistry.java
+++ src/main/java/co/uk/mrpineapple/additionalguns/core/registry/SoundRegistry.java
@@ -12,9 +12,14 @@ public class SoundRegistry {
     public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, AdditionalGuns.ID);
 
     /* Sounds */
-    public static final RegistryObject<SoundEvent> MAMMOTH_FIRE = register("item." + AdditionalGuns.ID + ".mammoth_fire");
-    public static final RegistryObject<SoundEvent> MAMMOTH_SUPPRESSED = register("item." + AdditionalGuns.ID + ".mammoth_suppressed");
-    public static final RegistryObject<SoundEvent> VAL_FIRE = register("item." + AdditionalGuns.ID + ".val_fire");
+    public static final RegistryObject<SoundEvent> MAMMOTH_FIRE = register("item.mammoth.fire");
+    public static final RegistryObject<SoundEvent> MAMMOTH_SUPPRESSED = register("item.mammoth.suppressed");
+    public static final RegistryObject<SoundEvent> VAL_FIRE = register("item.val.fire");
+    public static final RegistryObject<SoundEvent> AKM_FIRE = register("item.akm.fire");
+    public static final RegistryObject<SoundEvent> AK_ZERO_ONE_FIVE_FIRE = register("item.ak105.fire");
+    public static final RegistryObject<SoundEvent> AK_SEVEN_FOUR_FIRE = register("item.ak74.fire");
+    public static final RegistryObject<SoundEvent> AKS_SEVEN_FOUR_U = register("item.aks74u.fire");
+    public static final RegistryObject<SoundEvent> AK_SUPPRESSED = register("item.ak.suppressed");
 
     private static RegistryObject<SoundEvent> register(String name) {
         return SOUNDS.register(name, () -> new SoundEvent(new ResourceLocation(AdditionalGuns.ID, name)));
