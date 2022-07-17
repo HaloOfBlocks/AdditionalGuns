601c2fd28c3bd47c5de34d716f9099ed472bf90c
Autovw <68228006+Autovw@users.noreply.github.com>
2022-07-17 17:35:42 +0200

    ✨ Display information about bullet casings on the item

---
+++ src/main/java/co/uk/mrpineapple/additionalguns/core/item/BulletCasingItem.java
@@ -0,0 +1,34 @@
+package co.uk.mrpineapple.additionalguns.core.item;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+/**
+ * Author: Autovw
+ */
+public class BulletCasingItem extends Item {
+    public BulletCasingItem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level world, List<Component> tooltips, TooltipFlag flag) {
+        super.appendHoverText(stack, world, tooltips, flag);
+
+        if (!Screen.hasShiftDown()) {
+            tooltips.add(new TranslatableComponent("tooltip.additionalguns.hold_shift_key", new TextComponent("[SHIFT]").withStyle(ChatFormatting.YELLOW)));
+        } else {
+            tooltips.add(new TranslatableComponent("tooltip.additionalguns.bullet_casing_info").withStyle(ChatFormatting.RED));
+        }
+    }
+}
