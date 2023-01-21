afa0ad8c6dd41a42afbc985ce3cc84958db0d731
Autovw <68228006+Autovw@users.noreply.github.com>
2023-01-21 12:49:49 +0100

    ⚡ Apply cgmmeta format to zerkalo scope

--- src/main/java/haloofblocks/additionalguns/client/ClientHandler.java
+++ src/main/java/haloofblocks/additionalguns/client/ClientHandler.java
@@ -2,7 +2,7 @@ package haloofblocks.additionalguns.client;
 
 import com.mrcrayfish.guns.client.render.gun.model.SimpleModel;
 import haloofblocks.additionalguns.client.render.gun.model.KobraScopeModel;
-import haloofblocks.additionalguns.client.render.gun.model.ZerkaloScopeModel;
+//import haloofblocks.additionalguns.client.render.gun.model.ZerkaloScopeModel;
 import haloofblocks.additionalguns.core.registry.ItemRegistry;
 import com.mrcrayfish.guns.client.render.gun.ModelOverrides;
 //import com.mrcrayfish.guns.client.render.gun.model.LongScopeModel;
@@ -14,7 +14,7 @@ import com.mrcrayfish.guns.client.render.gun.ModelOverrides;
 public class ClientHandler {
     public static void registerModelOverrides() {
         //ModelOverrides.register(ItemRegistry.HOLO_SCOPE.get(), new ShortScopeModel());
-        ModelOverrides.register(ItemRegistry.ZERKALO_SCOPE.get(), new ZerkaloScopeModel());
+        //ModelOverrides.register(ItemRegistry.ZERKALO_SCOPE.get(), new ZerkaloScopeModel());
         ModelOverrides.register(ItemRegistry.KOBRA_SCOPE.get(), new KobraScopeModel());
         //ModelOverrides.register(ItemRegistry.ULTRA_LONG_SCOPE.get(), new LongScopeModel());
 
